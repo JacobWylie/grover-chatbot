92a4b9b605b78a315a860af0f97d07c8ad80f90b
JacobWylie <jacobwylie@gmail.com>
2018-01-17 19:33:33 +0100

    moved App.js to component folder

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import ChatBot from 'react-simple-chatbot';
-import steps from './bot/steps';
+import steps from '../bot/steps';
 
 class SimpleForm extends Component {
     render() {
